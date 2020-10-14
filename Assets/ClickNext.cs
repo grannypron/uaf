bbe6555235e18c536efa7db9265dfb07805b1cf4
Andrew Steeley <andrew_steeley@hotmail.com>
2020-10-14 18:05:58 -0500

    First Unity project commit

---
+++ Assets/ClickNext.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickNext : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject.Find("Title").SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0) || Input.GetMouseButtonUp(1) || Input.GetMouseButtonUp(2))
+        {
+            GameObject.Find("Title").SetActive(false);
+            GameObject.Find("MenuScreenFrame").SetActive(true);
+            Cursor.SetCursor((Texture2D)Resources.Load("cursor_Sword.png"), Vector2.zero, CursorMode.Auto);
+        }
+    }
+}
