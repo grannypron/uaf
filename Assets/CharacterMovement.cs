4a90dd82988d636fb165a557c0422316a365357f
Andrew Steeley <andrew_steeley@hotmail.com>
2020-10-28 20:08:58 -0500

    Managed to get a few monsters on the board and added collisions

---
+++ Assets/CharacterMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Tilemaps;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+            movePlayer(-1, 0);
+        if (Input.GetKeyDown(KeyCode.D))
+            movePlayer(1, 0);
+        if (Input.GetKeyDown(KeyCode.W))
+            movePlayer(0, 1);
+        else if (Input.GetKeyDown(KeyCode.S))
+            movePlayer(0, -1);
+
+    }
+
+    public void movePlayer(int xDir, int yDir)
+    {
+        float moveTime = 0.1f;
+        float inverseMoveTime = 1f / moveTime;
+        Rigidbody2D player = GetComponent<Rigidbody2D>();
+        Vector2 start = transform.position;
+        Vector2 end = start + new Vector2(xDir, yDir);
+        Vector3 newPostion = Vector3.MoveTowards(player.position, end, 1);
+        UnityEngine.Debug.Log(xDir + "/" + yDir);
+        //Call MovePosition on attached Rigidbody2D and move it to the calculated position.
+        player.MovePosition(newPostion);
+
+    }
+
+}
---
+++ Assets/CombatScreenEvents.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class CombatScreenEvents : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        paintMap();
+    }
+
+    void paintMap()
+    {
+        Rigidbody2D player = GetComponent<Rigidbody2D>();
+        Tilemap terrainTilemap = GameObject.Find("TerrainTilemap").GetComponent<Tilemap>();
+        Tilemap monsterTilemap = GameObject.Find("MonsterTilemap").GetComponent<Tilemap>();
+        Tile groundTile = (Tile)terrainTilemap.GetTile(new Vector3Int(-30, 11, 0));
+
+        UAFLib.Console console = new UAFLib.Console();
+        console.mJSPath = @"C:\Users\Shadow\Desktop\uaf.git\uaf-port\src\UAFLib\js";
+        UAFLib.ConsoleResults results = console.runTest(@"C:\Users\Shadow\Desktop\uaf.git\uaf-port\src\UAFLib\Tests\TestSimpleCombat.js");
+        object[] mapData = (object[])results.payload;
+
+        int combatMinX = -25;
+        int combatMinY = -25;
+        int combatWidth = 50;
+        int combatHeight = 50;
+        int combatMaxX = combatMinX + combatWidth;
+        int combatMaxY = combatMinY + combatHeight;
+
+        for (int x = combatMinX; x <= combatMaxX; x++)
+        {
+            for (int y = combatMinY; y <= combatMaxY; y++)
+            {
+                terrainTilemap.SetTile(new Vector3Int(x, y, 0), groundTile);
+            }
+        }
+        
+        int zIndex = 0;
+        for (int x = 0; x < mapData.Length; x++)
+        {
+            object[] column = (object[])mapData[x];
+            for (int y = 0; y < column.Length; y++)
+            {
+                int cellValue = System.Int32.Parse(((object)column[y]).ToString());
+                int[] translatedCoords = new int[] { x + combatMinX, y + combatMinY };
+                if (cellValue < 0)
+                {
+                    terrainTilemap.SetTile(new Vector3Int(translatedCoords[0], translatedCoords[1], zIndex), groundTile);
+                } else if (cellValue == 0)
+                {
+                    placePlayer(translatedCoords[0], translatedCoords[1]);
+                } else if (cellValue > 0)
+                {
+                    placeMonster(translatedCoords[0], translatedCoords[1], "monster" + cellValue, "Kobold");
+                }
+            }
+        }
+
+    }
+
+    private void placePlayer(int x, int y)
+    {
+        float centeringAdjustment = .5f;
+        Rigidbody2D player = GameObject.Find("Player").GetComponent<Rigidbody2D>();
+        player.MovePosition(new Vector2(x + centeringAdjustment, y + centeringAdjustment));
+    }
+
+    private void placeMonster(int x, int y, string id, string monsterType)
+    {
+        float centeringAdjustment = .5f;
+        GameObject monsterGO = new GameObject("Monster" + id);
+        SpriteRenderer renderer = monsterGO.AddComponent<SpriteRenderer>();
+        Tile monsterTile = Resources.Load<Tile>("Sprites/Monsters/" + monsterType);
+        renderer.sprite = monsterTile.sprite;
+        Rigidbody2D monsterR2D = monsterGO.AddComponent<Rigidbody2D>();
+        Transform scaleTransform = monsterGO.GetComponent<Transform>();
+        BoxCollider2D boxCollider2D = monsterGO.AddComponent<BoxCollider2D>();
+        scaleTransform.localScale = new Vector3Int(2, 2, 1);
+        monsterR2D.isKinematic = true;
+        monsterR2D.mass = 5;
+        monsterR2D.MovePosition(new Vector3(x + centeringAdjustment, y + centeringAdjustment));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
