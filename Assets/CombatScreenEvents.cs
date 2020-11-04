a6748332413609a6783f4e568f80435c95e65c5a
Andrew Steeley <andrew_steeley@hotmail.com>
2020-11-03 21:44:54 -0500

    Rolling back changes

--- Assets/CombatScreenEvents.cs
+++ Assets/CombatScreenEvents.cs
@@ -19,7 +19,7 @@ public class CombatScreenEvents : MonoBehaviour
     // Each square on the grid is 40x/y  Not sure why, but it is by eyeball
 
     private int PlayerScaleFactor = -1;
-    private const String CONFIG_FILE_URL = "https://grannypron.github.io/uaf/config.xml";
+    private const String CONFIG_FILE_URL = "https://raw.githubusercontent.com/grannypron/uaf/unity/config.xml";
     private Engine jintEngine;
     private ConsoleResults setupResults;
 
