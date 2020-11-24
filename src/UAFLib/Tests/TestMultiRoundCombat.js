8e39f2dc52ddca374d5fe82749f5a6fd08286715
Andrew Steeley <andrew_steeley@hotmail.com>
2020-11-23 20:17:33 -0500

    Just a little cleanup/simplification

--- src/UAFLib/Tests/TestMultiRoundCombat.js
+++ src/UAFLib/Tests/TestMultiRoundCombat.js
@@ -1,28 +1,15 @@
-
 Globals.SPECAB_HACKS = {};
-var setMonsterReady = -1;
-var callCount = 0;
-// This hack will make the m_isCombatReady flag set to each of them as set by the setMonsterReady variable
-Globals.SPECAB_HACKS["IsCombatReady"] = function (pkt) {
-    //Globals.debug("****SPECAB HACK: 1callCount:" + callCount);
-    if (setMonsterReady == -1) {
-        SPECAB.p_hook_parameters[0] = "";
-    } else {
-        //Globals.debug("****SPECAB HACK: callCount:" + callCount);
-        if (callCount == (setMonsterReady - 1)) {
-            SPECAB.p_hook_parameters[0] = "";
-            callCount = 0;
-        } else {
-            SPECAB.p_hook_parameters[0] = "1";
-            callCount++;
-        }
-    }
-    return CBRESULT.CBR_STOP;
-}
-
-
-
 Globals.SPECAB_HACKS["FreeAttack-CanFreeAttack"] = function (pkt) { SPECAB.p_hook_parameters[0] = "N"; }
+
+var originalIsDone = COMBATANT.prototype.IsDone;
+var combatantReady = -1;
+COMBATANT.prototype.IsDone = function (freeAttack, comment) {
+    //Override IsDone so that I can control who moves
+    if (this.self != combatantReady) {
+        return true;
+    }
+    return false;
+}
 
 // Move player away so that monsters move toward
 var cWarrior = combatData.m_aCombatants[0];
@@ -42,12 +29,14 @@ combatData.StartNewRound();  // Ends the turn we took in the last test case and 
 
 // Now let a monster move
 for (var idxMonster = 1; idxMonster < 4; idxMonster++) {
-    setMonsterReady = idxMonster;
-    combatData.UpdateCombat();          
+    combatantReady = idxMonster;
+    combatData.UpdateCombat();
     combatData.QComb.NotStartOfTurn();
     combatData.UpdateCombat();              // Why do I have to call this again?
     combatData.HandleCurrState(true);   
     var m_iDeathIndex = -1;
     combatData.HandleTimeDelayMsgBegin(0, m_iDeathIndex); // return from a timer pause that let the user see the roll/message
+    Globals.ASSERT(combatData.m_aCombatants[idxMonster].State() == individualCombatantState.ICS_Moving); 
     combatData.m_aCombatants[combatData.GetCurrCombatant()].EndTurn();
 }
+
